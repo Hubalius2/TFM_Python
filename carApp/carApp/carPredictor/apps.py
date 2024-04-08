63bf30679efc83ceea9c17b596a31f41c019d719
Hubalius2 <Pabmarsan2@gmail.com>
2024-04-08 22:48:46 +0200

    webApp

---
+++ carApp/carApp/carPredictor/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CarpredictorConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'carPredictor'
---
+++ carApp/carApp/carPredictor/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+
+def say_hello(request):
+    return HttpResponse('Hello, Django!')
+
+def welcome(request):
+    return render(request, 'welcome.html')
+
+def carApp(request):
+    return render(request, 'carApp.html')
